c02fc3ff768d95b5744a10b667e73cdb6b822ca9
md <71983818+md-exe@users.noreply.github.com>
2023-09-11 14:00:15 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using Quests.Models;
+using Quests.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Quests
+{
+    public partial class MainWindow : Window
+    {
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\ToDoData.json";
+        private BindingList<ToDoModel> _toDoData;
+        private IOService _ioService;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Закрыть программу
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        // Возможность двигать имитацию окна
+        private void ToolbarPanel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                base.OnMouseLeftButtonDown(e);
+                DragMove();
+            }
+        }
+        // Черная "Х" при наведении на кнопку закрытия
+        private void CloseButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            CloseLabel.Foreground = Brushes.Black;
+            Cursor = Cursors.Hand;
+        }
+        // Белая "Х" при отведении с кнопки закрытия
+        private void CloseButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            CloseLabel.Foreground = Brushes.White;
+            Cursor = Cursors.Arrow;
+        }
+        // Сворачивание окна
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Form.WindowState = WindowState.Minimized;
+        }
+        // Черный "-" при наведении на кнопку сворачивания
+        private void MinimizeButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            MinimizeLabel.Foreground = Brushes.Black;
+            Cursor = Cursors.Hand;
+        }
+        // Белая "-" при отведении с кнопки сворачивания
+        private void MinimizeButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            MinimizeLabel.Foreground = Brushes.White;
+            Cursor = Cursors.Arrow;
+        }
+        // Подгрузка данных списка
+        private void Form_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Получение данных из файла
+            _ioService = new IOService(PATH);
+            // Загрузка данных из файла
+            // Если успешно
+            try
+            {
+                _toDoData = _ioService.LoadData();
+            }
+            // Исключение
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Close();
+            }
+            // Отображение списка в DataGrid
+            ToDoList.ItemsSource = _toDoData;
+            // Функция при изменении списка
+            _toDoData.ListChanged += _toDoData_ListChanged;
+        }
+        // Проверка изменений списка
+        private void _toDoData_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            if (e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemDeleted || e.ListChangedType == ListChangedType.ItemChanged)
+            {
+                // Если успешно
+                try
+                {
+                    _ioService.SaveData(sender);
+                }
+                // Исключение
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    Close();
+                }
+            }
+        }
+        // Особо важные дела
+        private void ToDoList_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+    }
+}
