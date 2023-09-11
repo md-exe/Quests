bb4ff604adedc208c1a7d7d387a09b6749685c07
md <71983818+md-exe@users.noreply.github.com>
2023-09-11 14:02:53 +0300

    Add files via upload

---
+++ Models/ToDoModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quests.Models
+{
+    class ToDoModel: INotifyPropertyChanged
+    {
+        // Объявление переменных
+        private bool _isDone;
+        private string _tasktext;
+        // Время СЕЙЧАС
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        // Сделано
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set
+            {
+                if (_isDone == value)
+                {
+                    return;
+                }
+                else
+                {
+                    _isDone = value;
+                    OnPropertyChange("IsDone");
+                }
+                ;
+            }
+        }
+        // Текст задачи
+        public string TaskText
+        {
+            get { return _tasktext; }
+            set
+            {
+                if (_tasktext == value)
+                {
+                    return;
+                }
+                else
+                {
+                    _tasktext = value;
+                    OnPropertyChange("Task");
+                }
+                ;
+            }
+        }
+        // События изменения списка
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChange(string propertyName="")
+        {
+            // Проверка НЕ пустоты события
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Services/IOService.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using Quests.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quests.Services
+{
+    class IOService
+    {
+        // Объявление переменной пути
+        private readonly string PATH;
+        public IOService(string path)
+        {
+            PATH = path;
+        }
+        // Загрузка данных из файла
+        public BindingList<ToDoModel> LoadData()
+        {
+            // Проверка наличия файла
+            var fileExists = File.Exists(PATH);
+            if (!fileExists)
+            {
+                File.CreateText(PATH).Dispose();
+                return new BindingList<ToDoModel>();
+            }
+            // Открытие файла
+            using (var reader = File.OpenText(PATH))
+            {
+                var fileText = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<BindingList<ToDoModel>>(fileText);
+            }
+        }
+        // Сохранение списка
+        public void SaveData(object toDoData)
+        {
+            using (StreamWriter writer = File.CreateText(PATH))
+            {
+                string output = JsonConvert.SerializeObject(toDoData);
+                writer.WriteLine(output);
+                writer.Close();
+            }
+        }
+    }
+}
